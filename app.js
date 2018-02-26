aac3c3c564f6abf4dffd265dae40a6f2c5fc496b
milton <christodouloumilton@gmail.com>
2018-02-26 16:39:09 +0000

    globals

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//
+const amount = document.getElementById('amount');
+const interest = document.getElementById('interest');
+const years = document.getElementById('years');
+const monthly = document.getElementById('monthly-payment');
+const total = document.getElementById('total-payment');
+const total_int = document.getElementById('total-interest');
+
+
+
+
+
+
+
+document.querySelector('#loan-form').addEventListener('submit', calcResult);
+
+function calcResult() {
+
+	e.preventDefault();
+}
